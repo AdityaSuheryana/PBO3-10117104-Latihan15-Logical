a765a464275cca5b9079598cbb47ae697b8b8aa4
Aditya Suheryana <adityasuheryanaa@gmail.com>
2018-09-23 20:01:40 +0700

    first commit

---
+++ src/pbo3/pkg10117104/latihan15/logika/PBO310117104Latihan15Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117104.latihan15.logika;
+
+/**
+ * @author  
+ * Nama      : Aditya Suheryana
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Operator Logika 
+ */
+public class PBO310117104Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+(a && b));
+        System.out.println("a || b = "+(a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a&&b)" + !(a&&b));
+    }
+    
+}
